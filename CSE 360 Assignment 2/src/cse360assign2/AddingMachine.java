082d251ca8fdef6057f3f3f65764c387a9589a34
alex4 <alex4@DESKTOP-D51UR6V>
2019-10-09 21:05:47 -0700

    Initial commit of CSE 360 Assignment 2

---
+++ CSE 360 Assignment 2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * This method will add a value to the variable and then return 
+	 * 
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * The subtract method should subtract the parameter from the total variable
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * The getTotal method should return the current total 
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * A history of the transactions must be kept to be returned by the toString method. 
+	 * The history should start from the initial 0 value to the last input. 
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}

c0e5d6449256600821ad767e4ccaf3509ac74029
alex4 <alex4@DESKTOP-D51UR6V>
2019-10-09 23:15:44 -0700

    Fixed comments and completed all methods.

--- CSE 360 Assignment 2/src/cse360assign2/AddingMachine.java
+++ CSE 360 Assignment 2/src/cse360assign2/AddingMachine.java
@@ -1,44 +1,68 @@
+/* Alejandro Espinosa
+ * 1213082752
+ * CSE360
+ * Assignment 2
+ * AddingMachine.Java is a simple program that manipulates the value of a variable
+ * Using various methods. Also used to learn github and repositories.
+ */
+
 package cse360assign2;
 
-public class AddingMachine {
-
+public class AddingMachine 
+{
 	private int total;
+	private String transactionHistory;
 	
-	/**
-	 * This method will add a value to the variable and then return 
-	 * 
-	 */
-	public AddingMachine () {
+	public AddingMachine () 
+	{
+		//Start with values of 0
 		total = 0;  // not needed - included for clarity
+		transactionHistory = "0";
 	}
 	
 	/**
-	 * The subtract method should subtract the parameter from the total variable
+	 * The getTotal method returns the current total  
 	 */
-	public int getTotal () {
+	public int getTotal () 
+	{
 		return total;
 	}
 	
 	/**
-	 * The getTotal method should return the current total 
+	 *  The add method adds the parameter to the total variable
 	 */
-	public void add (int value) {
+	public void add (int value) 
+	{
+		total = total + value;
+		transactionHistory += " + " + value;
+	}
+	
+	/**
+	 * 
+	 * The subtract method subtracts the parameter from the total variable
+	 */
+	public void subtract (int value) 
+	{
+		total = total - value;
+		transactionHistory += " - " + value;
 		
 	}
 	
 	/**
-	 * A history of the transactions must be kept to be returned by the toString method. 
-	 * The history should start from the initial 0 value to the last input. 
+	 * A history of the transactions must be kept to be returned by the 
+	 * toString method.  The history should start from the initial 0 value to the last input. 
 	 */
-	public void subtract (int value) {
-		
-	}
-		
-	public String toString () {
-		return "";
+	public String toString () 
+	{
+		return transactionHistory;
 	}
 
-	public void clear() {
-	
+	/**
+	 * Method to reset the values of total and the transactionHistory. 
+	 */
+	public void clear() 
+	{
+		total = 0;
+		transactionHistory = "";
 	}
 }
